3ec8a61e9e073daf1dd1d860efd6883070c0ae6b
Miro Cekov <sudotestuser@gmail.com>
2014-01-31 15:35:54 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Munistar::Application.config.secret_key_base = '5af45bc4870f8e474117291486efdfd0f2de2f2a56cf3e75803aee304b5dd174e514c5c6231a310b07ccc7a5507d572fa2f9049f6022602033afb0a6350527e1'
